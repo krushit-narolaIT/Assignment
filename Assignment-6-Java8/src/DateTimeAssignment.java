0ad6c4b8e1583b62ae1d1b62de73c4ecc807c817
krushit-narolaIT <ksb@narola.email>
2025-02-03 09:53:09 +0530

    Adding Comments...

--- Assignment-6-Java8/src/DateTimeAssignment.java
+++ Assignment-6-Java8/src/DateTimeAssignment.java
@@ -5,14 +5,18 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeAssignment {
     public static void main(String[] args) {
         /*Q1. Create a program to calculate the number of days until your next birthday.*/
+        //get current time
         LocalDate today = LocalDate.now();
+        //get birthdate
         LocalDate birthday = LocalDate.of(2003, 10, 18);
 
+        //calculate age by diffyear and then add it to birthday
         if (today.isAfter(birthday)) {
             int diffYear = today.getYear() - birthday.getYear();
             birthday = birthday.plusYears(diffYear);
         }
 
+        //calculate period between today & birthday
         Period period = Period.between(today, birthday);
         System.out.println("Your Birthday in " + period.getMonths() + " Months " + period.getDays() + " Days");
 
--- Assignment-6-Java8/src/StreamAssignment.java
+++ Assignment-6-Java8/src/StreamAssignment.java
@@ -9,24 +9,30 @@ public class StreamAssignment {
         System.out.println("============================  Q1  =================================");
         /*Q1. Filter and Sort Lists with Streams Write a program to filter out
         strings shorter than 4 characters and sort the remaining strings alphabetically.*/
+
+        //list of countries
         List<String> countries = Arrays.asList("india", "australia", "oman", "china", "russia", "cuba", "iran");
         List<String> filterAndSortedStream = countries.stream()
-                .filter(country -> country.length() > 4)
-                .sorted()
+                .filter(country -> country.length() > 4) //filter countries with sort names
+                .sorted() //sort remaining words alphabetically
                 .toList();
         System.out.println("filterAndSortedStream :: " + filterAndSortedStream);
 
         System.out.println("============================  Q2  =================================");
         /*Q2. Summarize Numeric Data with reduce()
         Given a list of integers, find the product of all numbers using reduce().*/
+
+        //list of numbers
         List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
         int product = numbers.stream()
-                .reduce(1, (number1, number2) -> number1 * number2);
+                .reduce(1, (number1, number2) -> number1 * number2); //identity 1 which is initial values and then multiply each elements
         System.out.println("Product of all numbers :: " + product);
 
         System.out.println("============================  Q3  =================================");
         /*Q3.Group Objects by Category Create a Product class with name and category.
         Group products by their category using Collectors.groupingBy.*/
+
+        //product class
         class Product {
             String name;
             String category;
@@ -50,6 +56,7 @@ public class StreamAssignment {
             }
         }
 
+        //list of products
         List<Product> products = Arrays.asList(
                 new Product("TV", "Electronics"),
                 new Product("Bike", "Vehicle"),
@@ -59,20 +66,24 @@ public class StreamAssignment {
         );
 
         products.stream()
-                .collect(Collectors.groupingBy(prod -> prod.getCategory()))
+                .collect(Collectors.groupingBy(prod -> prod.getCategory())) //group products by their category
                 .forEach((category, items) -> System.out.println(category + ": " + items));
 
         System.out.println("============================  Q4  =================================");
 
         /*Q4.Experiment with Parallel Streams Compare the performance of sequential
         vs parallel streams for a CPU-intensive computation (e.g., calculating primes).*/
+
+        //list of integers
         List<Integer> integerList = IntStream.range(1, 999).boxed().collect(Collectors.toList());
         long start = System.currentTimeMillis();
+        //count prime numbers using sequencial stream
         long isPrimeSequencial = integerList.stream().filter(number -> StreamAssignment.isPrime(number)).count();
         long end = System.currentTimeMillis();
         System.out.println("Time taken by sequencial stream ::" + (end - start) + " ms");
 
         start = System.currentTimeMillis();
+        //count prime numbers using parallel stream
         long isPrimeParallel = integerList.parallelStream().filter(number -> StreamAssignment.isPrime(number)).count();
         end = System.currentTimeMillis();
         System.out.println("Time taken by Parallel stream ::" + (end - start) + " ms");
@@ -82,31 +93,38 @@ public class StreamAssignment {
         int productOfNumberInStream = IntStream.of(1, 2, 3, 4, 5)
                 .boxed()
                 .collect(Collector.of(
-                        () -> new int[]{1},
-                        (a, b) -> a[0] *= b,
-                        (a1, a2) -> {
+                        () -> new int[]{1}, //create new array for store product with size 1
+                        (a, b) -> a[0] *= b, //multiply each element with a[0]
+                        (a1, a2) -> { // for parallel stream
                             a1[0] *= a2[0];
                             return a1;
                         },
-                        a -> a[0]
+                        a -> a[0] //return product
                 ));
         System.out.println("Product: " + productOfNumberInStream);
 
         System.out.println("============================  Q6  =================================");
         /*Q6. flatMap vs map Use flatMap to process nested lists of strings, and compare with map.*/
+
+        //list of banned countries
         List<String> setBlocked = List.of("iraq", "syria", "moldova");
+
+        //list of all countries by their continent
         List<String> asianCountries = List.of("India", "Japan", "China", "Afghanistan", "Thailand", "Russia");
         List<String> europeanCountries = List.of("Germany", "Finland", "Croatia", "Czechia", "Luxumbarg", "Russia", "Turkey");
         List<String> middleEastCountries = List.of("Syria", "Egypt", "UAE", "Turkey");
 
+        //add all countries in one list<list>
         List<List<String>> countr = Arrays.asList(asianCountries, europeanCountries, middleEastCountries);
         System.out.println("==========Using Flatmap=============");
+        // this will store all countries in one single list
         countr.stream()
                 .flatMap(list -> list.stream())
                 .filter(str -> !setBlocked.contains(str))
                 .forEach(s -> System.out.print(" " + s));
 
         System.out.println("\n===========Using Map==============");
+        //this will not store all countries in single list instead it stores all list individually
         countr.stream()
                 .map(list -> list)
                 .forEach(System.out::println);
@@ -114,11 +132,19 @@ public class StreamAssignment {
         System.out.println("============================  Q7  =================================");
         /*Q7. Stream Performance Analyze the performance of spliterator
         and learn tips for efficient parallelization.*/
+        //numbers list with 1 to 99999
         List<Integer> numbersList = IntStream.rangeClosed(1, 999).boxed().toList();
+
+        //create stream from that list
         Stream<Integer> str1 = numbersList.stream();
 
+        //comparision starts
         long startNow = System.currentTimeMillis();
+
+        //split one half
         Spliterator<Integer> spliterator1 = str1.spliterator();
+
+        //split another half from splititerator1
         Spliterator<Integer> spliterator2 = spliterator1.trySplit();
 
         System.out.println("\nOutput from spliterator1: ");
@@ -143,6 +169,7 @@ public class StreamAssignment {
         /*Q8. Custom Collector for Aggregation Write a custom collector to
         aggregate employee salaries into a department-wise total.*/
 
+        //Employee.java
         class Employee {
             String name;
             String department;
@@ -166,14 +193,16 @@ public class StreamAssignment {
             }
         }
 
+        //list of employees
         List<Employee> employees = Arrays.asList(
                 new Employee("Krushit", "JAVA", 15000),
                 new Employee("D.Shah", "HR", 12000),
-                new Employee("Rahul", "PYTHON", 18000),
+                new Employee("Diven", ".NET", 15000),
                 new Employee("Vinay", "JAVA", 15000)
         );
 
-        Map<String, Integer> departmentSalary = employees.stream()
+        //
+        Map<String, Integer> departmentWiseSalary = employees.stream()
                 .collect(Collector.of(
                         HashMap::new,
                         (hashMap, employee) -> hashMap.merge(employee.getDepartment(), employee.getSalary(), Integer::sum),
@@ -183,7 +212,7 @@ public class StreamAssignment {
                         }
                 ));
 
-        System.out.println(departmentSalary);
+        System.out.println(departmentWiseSalary);
 
     }
 
